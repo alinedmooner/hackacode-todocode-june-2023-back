e651a98ac517ac2b82026b6f6a96cc8f67e5695f
Daniel M.C <daamorenoc@udistrital.edu.co>
2023-06-18 00:15:24 -0500

    ✨ feat: customer model

---
+++ src/models/customersModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const customerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    }
+  }
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;
