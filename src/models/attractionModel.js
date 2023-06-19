ff8ff493fb85684b89a43503f5cd1e73f73f157d
Daniel M.C <daamorenoc@udistrital.edu.co>
2023-06-19 17:22:35 -0500

    ✨ feat: attraction model

---
+++ src/models/attractionModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const attractionSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+);
+
+const Attraction = mongoose.model("Attraction", attractionSchema);
+
+export default Attraction;
