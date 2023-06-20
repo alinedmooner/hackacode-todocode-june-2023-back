1b7771cee90f59273d6f5d8401a0f487762afca1
Daniel M.C <daamorenoc@udistrital.edu.co>
2023-06-20 18:45:36 -0500

    🔥 feat: restart

--- src/controllers/employeeController.js
+++
@@ -1,9 +0,0 @@
-import asyncHandler from "../middlewares/asyncHandler.js";
-import Employee from "../models/employeeModel.js";
-
-const getEmployees = asyncHandler(async (req, res) => {
-  const employees = await Employee.find({});
-  res.json(employees);
-});
-
-export { getEmployees };
