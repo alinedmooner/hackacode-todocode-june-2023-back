14c2024f173cdaad2988031b0990d8890eea0061
Daniel M.C <alinedmooner@gmail.com>
2023-06-27 19:05:56 -0500

    ✨ fix: index

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import attractionRoutes from "./routes/attractionRoutes.js";
+import employeesRoutes from "./routes/employeeRoutes.js";
+import customerRoutes from "./routes/customerRoutes.js";
 
 dotenv.config();
 
 connectDB();
 
-const port = process.env.PORT || 5273;
+const port = process.env.PORT || 6173;
 
 const app = express();
 
-app.get("/api/", (req, res) => {
-  res.send("Infinity Dream Park API");
-});
+app.use("/api/attractions", attractionRoutes);
+
+app.use("/api/employees", employeesRoutes);
+
+app.use("/api/customers", customerRoutes);
 
 app.use(notFound);
 
